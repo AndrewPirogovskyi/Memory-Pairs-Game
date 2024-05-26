13e7d3ea2855ccde221956c98b48b64511b95aac
Andrew Pirogovskyi <72497767+AndrewPirogovskyi@users.noreply.github.com>
2024-05-26 20:32:57 +0300

    Sixth commit

--- Scripts/script_en.js
+++ Scripts/script_en.js
@@ -1,5 +1,6 @@
 var myCards = document.getElementById('container');
 var resultsArray = [];
+var flippedCards = [];
 var counter = 0;
 var text = document.getElementById('text');
 var seconds = 0;
@@ -8,32 +9,25 @@ var appendTens = document.getElementById("tens");
 var appendSeconds = document.getElementById("seconds");
 var Interval;
 var images = [
-    'sass',
-    'git',
-    'gulp',
-    'css',
-    'grunt'
+    '1', '2', '3', '4', '5',
+    '6', '7', '8', '9', '10'
 ];
-var clone = images.slice(0); // duplicate array
-var cards = images.concat(clone); // merge two arrays
-
-// Double the number of cards for each item
-cards = cards.concat(cards);
+var cards = images.concat(images); // Create pairs
 
 // Shuffle function
 function shuffle(o) {
     for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
     return o;
 }
+
 function startGame() {
-    
     // Hide menu
     document.getElementById('menu').style.display = 'none';
-    document.getElementById('clicktoStart').style.display='block';
-    
+    document.getElementById('clicktoStart').style.display = 'block';
+
     // Shuffle cards
     shuffle(cards);
-    
+
     // Create cards
     for (var i = 0; i < cards.length; i++) {
         var card = document.createElement('div');
@@ -41,31 +35,40 @@ function startGame() {
         card.dataset.view = "card";
         myCards.appendChild(card);
         card.onclick = function () {
-            if (this.className != 'flipped' && this.className != 'correct') {
+            if (flippedCards.length < 2 && this.className !== 'flipped' && this.className !== 'correct') {
                 this.className = 'flipped';
+                flippedCards.push(this);
                 var result = this.dataset.item;
                 resultsArray.push(result);
                 clearInterval(Interval);
                 Interval = setInterval(startTimer, 10);
-            }
-            if (resultsArray.length > 1) {
-                if (resultsArray[0] === resultsArray[1]) {
-                    check("correct");
-                    counter++;
-                    win();
-                    resultsArray = [];
-                } else {
-                    check("reverse");
-                    resultsArray = [];
+                if (flippedCards.length === 2) {
+                    setTimeout(compareCards, 700); // Adjust this timeout as needed for card flipping animation duration
                 }
             }
         };
     }
 }
+
+function compareCards() {
+    if (resultsArray[0] === resultsArray[1]) {
+        for (var i = 0; i < flippedCards.length; i++) {
+            flippedCards[i].className = 'correct';
+        }
+        counter++;
+        win();
+    } else {
+        for (var i = 0; i < flippedCards.length; i++) {
+            flippedCards[i].className = 'reverse';
+        }
+    }
+    resultsArray = [];
+    flippedCards = [];
+}
+
 function startTimer() {
     document.getElementById('title').style.display = 'none';
-    
-    text.innerHTML = 'Current time ' + seconds + ":" + tens;
+
     tens++;
     if (tens < 9) {
         appendTens.innerHTML = "0" + tens;
@@ -83,20 +86,12 @@ function startTimer() {
         appendSeconds.innerHTML = seconds;
     }
 }
-function check(className) {
-    var x = document.getElementsByClassName("flipped");
-    setTimeout(function () {
-        for (var i = (x.length - 1); i >= 0; i--) {
-            x[i].className = className;
-        }
-    }, 500);
-}
+
 function win() {
     if (counter === 10) {
         clearInterval(Interval);
-        text.innerHTML = "Your time was " + seconds + ":" + tens;
         // Display congratulatory message
-        document.getElementById('congratulations').style.display = 'block';
+        document.getElementById('congratulations').style.display = 'flex';
     }
 }
 
